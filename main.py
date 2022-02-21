09eac51f801401b41e2676ae56279185b938f847
Tomáš <kretom84@gmail.com>
2022-02-21 20:39:32 +0100

    binary input

--- main.py
+++ main.py
@@ -28,32 +28,48 @@ device_app = BAC0.lite(ip="192.168.1.40/24", port=47808, deviceId=101)
 
 time.sleep(1)
 _new_objects = analog_input(
-    instance=0,
-    name="RT",
-    properties={"units": "degreesCelsius"},
-    description="Room Temperature",
-    presentValue=17,
-)
+        instance=0,
+        name="RT",
+        properties={"units": "degreesCelsius"},
+        description="Room Temperature",
+        presentValue=17,
+    )
 _new_objects = analog_value(
-    instance=0,
-    name="VT",
-    properties={"units": "degreesCelsius"},
-    description="Wanted Temperature",
-    presentValue=22,
-)
+        instance=0,
+        name="VT",
+        properties={"units": "degreesCelsius"},
+        description="Wanted Temperature",
+        presentValue=22,
+    )
+_new_objects = binary_input(
+        instance=0,
+        name="BI",
+        description="Device ON/OFF",
+        presentValue=True,
+    )
+
 
 _new_objects.add_objects_to_application(device_app)
 
 i = 0
-previousTemperature = 17
+previousTemperature = actualTemperature = device_app["RT"].presentValue
 counter = 0
 randCounter = random.randint(45, 90)
+
 
 while True:
     actualTemperature = device_app["RT"].presentValue
     wantedTemperature = device_app["VT"].presentValue
+    devState = device_app["BI"].presentValue
+
     print(actualTemperature)
-    temp = previousTemperature + 0.05 * (wantedTemperature - actualTemperature)
+    print(devState)
+
+    if devState != "inactive":
+        temp = previousTemperature + 0.05 * (wantedTemperature - actualTemperature)
+    else:
+        temp = actualTemperature - 0.2
+
 
     if counter < randCounter:
         counter += 1
@@ -72,8 +88,17 @@ while True:
     if randCounter > 70:
         rand = -rand
 
+
     device_app["RT"].presentValue = temp - rand
+
+
     previousTemperature = temp
     time.sleep(1)
+
+
+
+
+
+
 
 input("prompt: ")
